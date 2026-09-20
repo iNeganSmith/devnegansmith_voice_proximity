60373150c703d2b36a774327cf69d5e4006f486e
RyuKazu <pacheco97j@gmail.com>
2026-09-20 11:00:16 -0500

    chore: prepare v1.0.2 for FiveM asset delivery

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,14 @@ lua54 'yes'
 
 author 'DevNeganSmith'
 description 'Indicador visual de proximidad para pma-voice. Compatible con ESX Legacy, QBCore, Qbox/QBX y Standalone.'
-version '1.0.1'
+version '1.0.2'
 
 dependency 'pma-voice'
 
 shared_script 'config.lua'
 client_script 'client.lua'
+
+-- Mantiene la configuración editable cuando el recurso se distribuye mediante FiveM Asset Escrow.
+escrow_ignore {
+    'config.lua'
+}
